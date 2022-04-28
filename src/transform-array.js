d38026cf171b1ecbfac5656c25b85879e8389edb
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-28 22:20:18 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // if (Array.isArray(arr) === false) {
+  //   throw new Error("\'arr\' parameter must be an instance of the Array!");
+  // }
+  const nextDel = '--discard-next';
+  const prevDel = '--discard-prev';
+  const nextDub = '--double-next';
+  const prevDub = '--double-prev';
+  let arr2 = arr;
+  let newArr = [];
+
+  if (Array.isArray(arr)) {
+    arr2.map((el, ind) => {
+      if (el === nextDel) {
+        arr2.splice(ind + 1, 1);
+
+      } else if (el === prevDel) {
+        typeof arr2[ind - 1] !== 'string' ? newArr.pop() : null;
+      } else if (el === nextDub) {
+        ind + 1 < arr2.length ? newArr.push(arr2[ind + 1]) : null;
+      } else if (el === prevDub) {
+        typeof arr2[ind - 1] !== 'string' && arr2[ind - 1] !== undefined ? newArr.push(arr2[ind - 1]) : null;
+      } else {
+        newArr.push(el)
+      }
+    })
+    return newArr;
+  } else {
+    throw new Error("\'arr\' parameter must be an instance of the Array!");
+  }
+
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
+
+
+
 
 module.exports = {
   transform
