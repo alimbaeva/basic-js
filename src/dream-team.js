2e02f55fc02a54fc6aa98ebce9949b018efe94da
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-28 15:03:58 +0300

    feat:dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (members === null || members === undefined) { return false }
+
+  let name = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      /\s/.test(members[i]) ? name += members[i].replace(/\s/g, '')[0] : name += members[i][0]
+    }
+  }
+  let n2 = name.toUpperCase().split('').sort((a, b) => a.charCodeAt() - b.charCodeAt())
+  return n2.join('')
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
-
 module.exports = {
   createDreamTeam
 };
