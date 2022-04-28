bea1d6ddd5904261c8795d0db19b555094e245b8
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-28 08:47:39 +0300

    feat: common-character-count delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,8 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  let nambersBegen = String(n).split('');
+  let namber = [];
+
+  for (let i = 0; i < nambersBegen.length; i++) {
+    let n = ''
+    for (let y = 0; y < nambersBegen.length; y++) {
+      if (i !== y) {
+        n += nambersBegen[y]
+      }
+    }
+    namber.push(Number(n))
+  }
+  let mainNamber = namber.sort((a, b) => a - b)
+  return mainNamber[mainNamber.length - 1]
+
+
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
