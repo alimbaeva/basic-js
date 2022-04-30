dcb93815e08868aaddd96965ca6cde34a89e38dc
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-30 08:02:54 +0300

    feat: dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+
+
+function getDNSStats(domains) {
+  let obj = {};
+  for (let i = 0; i < domains.length; i++) {
+    let domLen = domains[i].split('.').reverse();
+    let domLen1 = [];
+    let name = ''
+    for (let y = 0; y < domLen.length; y++) {
+      if (y === 0) {
+        domLen1.push(`.${domLen[y]}`)
+      } else {
+        for (let q = 0; q <= y; q++) {
+          name += `.${domLen[q]}`
+        }
+        domLen1.push(name);
+        name = ''
+      }
+    }
+
+    for (let key of domLen1) {
+      if (obj[key]) {
+        obj[key]++;
+      } else {
+        obj[key] = 1;
+      }
+    }
+  }
+  return obj
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
+
 
 module.exports = {
   getDNSStats
