98969192ac08bcb3c2cf3810c1b73bf463773530
Asel <asel.alimbaeva.kk@gmail.com>
2023-12-15 14:04:28 +0300

    refactore: dateSample getCommonCharacterCount

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,21 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  s1 = s1.split('');
-  s2 = s2.split('');
+  const setS1 = new Set(s1);
   let count = 0;
 
-  for (let i = 0; i < s1.length; i++) {
-    if (s2.indexOf(s1[i]) !== -1) {
-      count++;
-      s2.splice(s2.indexOf(s1[i]), 1)
-    }
+  for (const char of setS1) {
+    const occurrencesInS1 = s1.split(char).length - 1; //'aabcc'.split(char) => ['', '', 'bcc'].length => 3-1 => 2
+    const occurrencesInS2 = s2.split(char).length - 1;
+    count += Math.min(occurrencesInS1, occurrencesInS2); // Math.min(2, 3) => 2;
   }
 
   return count;
-
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
 }
 
 module.exports = {
