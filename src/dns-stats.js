93e91e34efa5e8ad9a06a0e079a5bd562c3d9bdb
Asel <asel.alimbaeva.kk@gmail.com>
2023-12-15 18:27:25 +0300

    refactore: getDNSStats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -25,34 +25,21 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 
 function getDNSStats(domains) {
-  let obj = {};
-  for (let i = 0; i < domains.length; i++) {
-    let domLen = domains[i].split('.').reverse();
-    let domLen1 = [];
-    let name = ''
-    for (let y = 0; y < domLen.length; y++) {
-      if (y === 0) {
-        domLen1.push(`.${domLen[y]}`)
-      } else {
-        for (let q = 0; q <= y; q++) {
-          name += `.${domLen[q]}`
-        }
-        domLen1.push(name);
-        name = ''
-      }
-    }
+  const obj = {};
 
-    for (let key of domLen1) {
-      if (obj[key]) {
+  domains.forEach(el => {
+    const newArr = el.split('.').reverse();
+    for(let i =0; i < newArr.length; i++) {
+      const key = `.${newArr.slice(newArr[i], i+1).join('.')}`;
+      if (obj.hasOwnProperty(key)) {
         obj[key]++;
       } else {
         obj[key] = 1;
       }
     }
-  }
-  return obj
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  });
+
+  return obj;
 }
 
 
