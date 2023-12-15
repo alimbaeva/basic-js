6741fbcffa83f0b060549928f4b9a51dd137d71c
Asel <asel.alimbaeva.kk@gmail.com>
2023-12-15 21:33:09 +0300

    refactore: encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,33 +11,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function encodeLine(str) {
-  if (!str) { return str }
-  str = str.split('')
-  let len = '';
-
-  let count = 0;
-  for (let i = 0; i <= str.length; i++) {
-    for (let y = 0; y < str.length; y++) {
-      if (str[0] === str[y]) {
-        count++
-      } else {
-
-        break
-      }
-
-    }
-    len += count - 1 === 0 || count === 0 ? str[0] : count + str[0]
-    str.splice(0, count)
-    count = 0
-    i = 0
-
-  }
-  return len
-
-
-
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (!str) return str;
+  const regex = /(.)\1*/g;
+  const matches = str.match(regex);
+  let strings = '';
+  matches.forEach(el => strings += `${el.length === 1 ? '' : el.length}${el[0]}`);
+  return strings;
 }
 
 
