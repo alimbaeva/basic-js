6009bbc44c5220b1bf08df4081e197073934ed79
Asel <asel.alimbaeva.kk@gmail.com>
2023-12-16 10:40:56 +0300

    refactore: repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -14,54 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
+ * 
+ * https://stackoverflow.com/questions/34937349/javascript-create-empty-array-of-a-given-size
+ * https://medium.com/@olga.myazina/5-способов-создать-массив-в-javascript-602dc27fd20
+ * 
+ * fill => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
  */
 function repeater(str, options) {
-  const str1 = typeof str === 'string' ? str : String(str);
-  const addition1 = String(options.addition) !== 'undefined' ? typeof options.addition === 'string' ? options.addition : String(options.addition) : null;
-  const repeatTimes1 = options.repeatTimes ? options.repeatTimes : 1;
-  const additionRepeatTimes1 = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
-  const separator1 = options.separator ? options.separator : '+';
-  const additionSeparator1 = options.additionSeparator ? options.additionSeparator : '|';
+  const str1 = String(str);
+  const addition1 = String(options.addition) !== 'undefined' ? String(options.addition) : '';
+  const repeatTimes1 = options.repeatTimes || 1;
+  const additionRepeatTimes1 = options.additionRepeatTimes || 1;
+  const separator1 = options.separator || '+';
+  const additionSeparator1 = options.additionSeparator || '|';
 
+  const additionRep = Array(additionRepeatTimes1).fill(addition1).join(additionSeparator1); // Array(3) => [ , , ,].fill('oo') => ['oo', 'oo', 'oo'].join('+') => "oo+oo+oo";
+  const strRep = Array(repeatTimes1).fill(str1 + additionRep).join(separator1);
 
-  if (addition1) {
-    let additionRep = Array(additionRepeatTimes1).fill(addition1);
-    let additionRep2 = '';
-    for (let i = 0; i < additionRep.length; i++) {
-      i === additionRep.length - 1 ? additionRep2 += `${additionRep[i]}` : additionRep2 += `${additionRep[i]}${additionSeparator1}`;
-    }
-
-    let strRep = Array(repeatTimes1).fill(str1 + additionRep2);
-    let strRep2 = '';
-    for (let i = 0; i < strRep.length; i++) {
-      i === strRep.length - 1 ? strRep2 += `${strRep[i]}` : strRep2 += `${strRep[i]}${separator1}`;
-    }
-    return strRep2;
-  }
-
-
-
-  let strRep = Array(repeatTimes1).fill(str1);
-  let strRep2 = '';
-  for (let i = 0; i < strRep.length; i++) {
-    i === strRep.length - 1 ? strRep2 += `${strRep[i]}` : strRep2 += `${strRep[i]}${separator1}`;
-
-  }
-
-  return strRep2
-
-
-
-
-
-
-
-
-
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  return strRep;
 }
-
 
 module.exports = {
   repeater
